0dd057f106d5e6ac8c7a2dc178a877e9ff9bb005
pmahindru <64604941+pmahindru@users.noreply.github.com>
2021-05-07 22:25:30 -0300

    Delete mahindru directory

--- mahindru/function.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// require_once 'serverlogin.php';
-	// $conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
-	// if ($conn->connect_error) {
-	// 	die("Connection failed!" . mysqli_connect_error()); 
-	// }
-	// mysqli_select_db($conn, $db_database) or die("Unable to select database: " . mysql_error());
-?>
--- mahindru/serverlogin.php
+++
@@ -1,7 +0,0 @@
-<?php // server login.php 
-
-  // 	$db_hostname = 'db.cs.dal.ca';  
- 	// $db_username = 'mahindru';  //your username
-  // 	$db_password = 'TeiUXDFigbRTcDkJh9qfkrW5R'; //your password
-  //   $db_database = 'mahindru'; //database name
-?>
